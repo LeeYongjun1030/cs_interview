b21ad84a00c7999b9f045bacd2a1fb9c20b32e58
LeeYongjun1030 <yiyj1030@gmail.com>
2026-01-22 23:40:03 +0900

    chore: configure native Android/iOS settings (Firebase, Permissions, Build targets)

---
+++ android/app/src/main/kotlin/com/yongjunlee/cs_interview/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.yongjunlee.cs_interview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -19,21 +19,67 @@ class DefaultFirebaseOptions {
     if (kIsWeb) {
       return web;
     }
-    // TODO: Add other platforms (Android/iOS) if needed later
-    throw UnsupportedError(
-      'DefaultFirebaseOptions are not supported for this platform.',
-    );
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        return macos;
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
   }
 
-  // TODO: Replace these values with your actual Firebase config
-  // You can find them in Firebase Console > Project Settings > General > Your Apps > Web App
   static const FirebaseOptions web = FirebaseOptions(
     apiKey: 'AIzaSyBb2SIig2GDZKvl560MBMDa6cIsVGPdqww',
     appId: '1:936919588774:web:4ec483d76ec1a39db0b967',
     messagingSenderId: '936919588774',
-    projectId: 'cs-interview-66fb7', // You already provided this
+    projectId: 'cs-interview-66fb7',
     authDomain: 'cs-interview-66fb7.firebaseapp.com',
     storageBucket: 'cs-interview-66fb7.firebasestorage.app',
-    measurementId: 'G-R5H5XRV4MS', // Optional
+    measurementId: 'G-R5H5XRV4MS',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyCfSuP22XmCR7xqyL0ZlWoJ3whdTvMl8JY',
+    appId: '1:936919588774:android:2522cb1d37a50337b0b967',
+    messagingSenderId: '936919588774',
+    projectId: 'cs-interview-66fb7',
+    storageBucket: 'cs-interview-66fb7.firebasestorage.app',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyChg5KUgPtB25Jxyic16vBx6nQYcwLkUn4',
+    appId: '1:936919588774:ios:c4230f0a9ee2d940b0b967',
+    messagingSenderId: '936919588774',
+    projectId: 'cs-interview-66fb7',
+    storageBucket: 'cs-interview-66fb7.firebasestorage.app',
+    iosClientId:
+        '936919588774-i8f9q82tqe5aoak31biov3dopcc8tfrq.apps.googleusercontent.com',
+    iosBundleId: 'com.yongjunlee.csInterview',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyChg5KUgPtB25Jxyic16vBx6nQYcwLkUn4',
+    appId: '1:936919588774:ios:c4230f0a9ee2d940b0b967',
+    messagingSenderId: '936919588774',
+    projectId: 'cs-interview-66fb7',
+    storageBucket: 'cs-interview-66fb7.firebasestorage.app',
+    iosClientId:
+        '936919588774-i8f9q82tqe5aoak31biov3dopcc8tfrq.apps.googleusercontent.com',
+    iosBundleId: 'com.yongjunlee.csInterview',
   );
 }
